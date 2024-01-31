7d7feb23602fc2e666d615ba6719fc86434c5c87
root <root@jump.ranzai.store>
2024-01-31 06:02:36 +0000

    fix build errors

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module rcong-cli
 
 go 1.21.6
+
+require github.com/FlyingRadish/rcong v1.0.1 // indirect
--- main.go
+++ main.go
@@ -9,17 +9,16 @@ import (
 
 
 func main() {
-	// 定义命令行参数
 	ip := flag.String("ip", "", "server ip")
-	port := flag.Int("p", "", "server port")
+	port := flag.Int("p", 25575, "server port")
 	pwd := flag.String("pwd", "", "auth password")
 	command := flag.String("c", "", "rcon command")
 	flag.Parse()
-	conn := rcong.NewRCONConnection(ip, port, pwd, 3, 10)
+	conn := rcong.NewRCONConnection(*ip, *port, *pwd, 3, 10)
 	conn.Connect()
 	defer conn.Close()
 
-	response, err := conn.ExecCommand(command)
+	response, err := conn.ExecCommand(*command)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
